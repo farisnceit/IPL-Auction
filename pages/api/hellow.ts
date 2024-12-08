58e56e80f117bb9e0e64f32b50aac9f8055f9e91
Mohamed Fariz <faris872010@gmail.com>
2024-12-08 13:55:57 +0530

    code corrected

--- pages/api/hellow.ts
+++ pages/api/hellow.ts
@@ -1,11 +1,18 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
- 
+import type { NextApiRequest, NextApiResponse } from "next";
+
 type ResponseData = {
-  message: string
-}
- 
+  message: string;
+};
+
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<ResponseData>
+  res: NextApiResponse<ResponseData>,
 ) {
-  res.status(200).json({ message: 'Hello from Next.js!' })
+  // Handle GET requests
+  if (req.method === "GET") {
+    res.status(200).json({ message: "Hello from Next.js!" });
+  } else {
+    // Method Not Allowed
+    res.status(405).json({ message: "Method Not Allowed" });
+  }
+}
